a1020136eae30e0139053135369649da7a4ef75b
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-27 23:06:13 +0200

    create model and add contente to component

--- FirstApp/src/app/recipes/recipes-list/recipes-list.component.ts
+++ FirstApp/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipies.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe('a test recipies', 
+                'This is simply a test', 
+                'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1200,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
+  ];
 
   constructor() { }
 
