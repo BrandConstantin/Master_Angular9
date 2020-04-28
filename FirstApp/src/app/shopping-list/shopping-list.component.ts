f4b348b0c98868ff0a77eebec154645664cd0539
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-28 23:44:54 +0200

    Revert "add buttons for the second model"

--- FirstApp/src/app/shopping-list/shopping-list.component.ts
+++ FirstApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,10 +6,6 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apple', 5),
-    new Ingredient('Tomatoes', 4)
-  ];
 
   constructor() { }
 
